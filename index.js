9dd207ae37bfc887c77dfda434691f718ebede6c
李杨 <1711467488@qq.com>
2018-02-27 19:19:41 +0800

    no message

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false
+  });
   const page = await browser.newPage();
   page.setViewport({
-    width: 1920,
-    height: 1080
+    width: 1366,
+    height: 768
   })
-  await page.goto('https://github.com/FairyEver');
+  await page.goto('https://cn.vuejs.org/');
   await page.screenshot({
     path: 'screenshots/screenshots.png',
     fullPage: true
