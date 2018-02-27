ad8462b73eb921a61f1295d25be451d95b4eddf1
李杨 <1711467488@qq.com>
2018-02-27 16:54:48 +0800

    no message

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://github.com/FairyEver/study-puppeteer');
-  await page.screenshot({path: 'example.png'});
-
+  page.setViewport({
+    width: 1920,
+    height: 1080
+  })
+  await page.goto('https://www.baidu.com/');
+  await page.screenshot({path: 'screenshots/example.png'});
   await browser.close();
 })()
